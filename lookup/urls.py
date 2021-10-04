25e9ac9b909fd84aadff9c33a5e1a92fb6f11462
pochs <ochs.pascal@gmail.com>
2021-10-04 07:39:50 +0100

    initial commit

---
+++ lookup/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views # The period stands for right here.
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+]
