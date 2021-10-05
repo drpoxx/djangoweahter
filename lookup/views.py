6ea2c011ea359f3381168d7b74f1ed40a1127954
pochs <ochs.pascal@gmail.com>
2021-10-05 17:30:37 +0100

    api request and error handling

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    # Create the api request:
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=73101&distance=50&API_KEY=35D35F74-7912-40CE-98DB-3DD932F6960C")
+
+    # Error handling of the api request:
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Errror..."
+
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
